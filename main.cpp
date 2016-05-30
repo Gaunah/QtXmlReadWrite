5c9bc897745d2bd3825f156a5f3145ce03581eba
gaunah <christoph@zelinski.de>
2016-05-30 17:26:21 +0200

    WIP - main to test PlayerInfoReader

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
+#include "playerinforeader.h"
+
+#include <QFile>
+#include <QDebug>
+#include <QDir>
 
 int main(int argc, char **argv)
 {
+    Q_UNUSED(argc)
+    Q_UNUSED(argv)
 
+    QDir dir  = QDir::current();
+    dir.cd("../QtSerialization");
+    QString path = dir.absolutePath() + "/PlayerInfo.xml";
+
+    if (!QFile::exists(path)) {
+        qDebug() << "file:" << path << "don't exsits.";
+        return 1;
+    }
+
+    QFile file(path);
+    file.open(QFile::ReadOnly | QFile::Text);
+    PlayerInfoReader pir(&file);
+
+    if (pir.read()) {
+        PlayerInfo playerInfo = pir.result();
+        //TODO
+    } else {
+        qDebug() << "Could not read file";
+    }
     return 0;
 }
